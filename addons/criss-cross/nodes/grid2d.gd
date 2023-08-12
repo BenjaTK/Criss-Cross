db525e719ba58e123d1f9baa7dd85b194773d39e
BenjaTK <73806216+BenjaTK@users.noreply.github.com>
2023-08-12 13:35:50 -0300

    Add infinite properties and hide grid to private
    Closes #6

--- addons/criss-cross/nodes/grid2d.gd
+++ addons/criss-cross/nodes/grid2d.gd
@@ -2,51 +2,74 @@
 extends Node2D
 
 
-@export var size: Vector2i = Vector2i(16, 16) : set = set_size
+@export var infinite: bool = false :
+	set(value):
+		infinite = value
+		reset()
+@export var regions: Array[Rect2i] = [Rect2i(0, 0, 16, 16)] :
+	set(value):
+		regions = value
+		reset()
+## If true, ignores [param regions] and doesn't initialize with any cells.
 @export var cell_size: Vector2i = Vector2i(16, 16) :
 	set(value):
 		cell_size = value
 		queue_redraw()
 @export_group("Debug", "debug_")
-## Draws the grid when in-game.
-## Will draw the grid in-editor if the node is selected.
+## Draws the _grid when in-game.
+## Will draw the _grid in-editor if the node is selected.
 @export var debug_enabled := false :
 	set(value):
 		debug_enabled = value
 		queue_redraw()
-@export var debug_grid_color := Color("ffffffbf") :
+@export var debug__grid_color := Color("ffffffbf") :
 	set(value):
-		debug_grid_color = value
+		debug__grid_color = value
 		queue_redraw()
 @export var debug_show_labels := true :
 	set(value):
 		debug_show_labels = value
 		queue_redraw()
+@export var debug_font: Font :
+	set(value):
+		if value == null:
+			var label = Label.new()
+			value = label.get_theme_font("")
+			label.queue_free()
+		debug_font = value
+		queue_redraw()
+@export_range(0, 1, 1, "or_greater") var debug_font_size: float = 12.0 :
+	set(value):
+		debug_font_size = value
+		queue_redraw()
 
-var grid: Dictionary
-var _debug_font: Font
+var _grid: Dictionary
 
 
 func _ready() -> void:
-	var label = Label.new()
-	_debug_font = label.get_theme_font("")
-	label.queue_free()
+	if debug_font == null:
+		var label = Label.new()
+		debug_font = label.get_theme_font("")
+		label.queue_free()
 
 	reset()
-
-
-func set_size(value: Vector2i) -> void:
-	size = value
-	reset()
-	queue_redraw()
 
 
 func reset() -> void:
-	grid.clear()
+	_grid.clear()
 
-	for x in size.x:
-		for y in size.y:
-			grid[Vector2i(x, y)] = null
+	if infinite:
+		queue_redraw()
+		return
+
+	for region in regions:
+		var x_range = range(region.position.x, region.position.x + region.size.x)
+		var y_range = range(region.position.y, region.position.y + region.size.y)
+		for x in x_range:
+			for y in y_range:
+				_grid[Vector2i(x, y)] = null
+
+	queue_redraw()
 
 
 # --- Values ---
@@ -54,11 +77,11 @@ func reset() -> void:
 
 ## Sets [param cell] to [param value]
 func set_value(cell: Vector2i, value) -> void:
-	if not has_cell(cell):
+	if not has_cell(cell) and not infinite:
 		push_warning("%s is out of %s's bounds" % [cell, name])
 		return
 
-	grid[cell] = value
+	_grid[cell] = value
 
 
 func set_valuexy(x: int, y: int, value) -> void:
@@ -73,10 +96,11 @@ func set_values(cells: Array[Vector2i], value) -> void:
 
 func get_value(cell: Vector2i):
 	if not has_cell(cell):
-		push_warning("%s is out of %s's bounds" % [cell, name])
+		if not infinite:
+			push_warning("%s is out of %s's bounds" % [cell, name])
 		return null
 
-	return grid[cell]
+	return _grid[cell]
 
 
 func get_valuexy(x: int, y: int):
@@ -85,12 +109,29 @@ func get_valuexy(x: int, y: int):
 
 # --- Checks ---
 
+
 func has_cell(cell: Vector2i) -> bool:
-	return grid.keys.has(cell)
+	return _grid.keys.has(cell)
 
 
 func has_cellxy(x: int, y: int) -> bool:
 	return has_cell(Vector2i(x, y))
+
+
+func is_value_null(cell: Vector2i) -> bool:
+	return get_value(cell) == null
+
+
+func is_value_nullxy(x: int, y: int) -> bool:
+	return is_value_null(Vector2i(x, y))
+
+
+func are_values_null(cells: Array[Vector2i]) -> bool:
+	for cell in cells:
+		if not is_value_null(cell):
+			return false
+
+	return true
 
 
 # --- Positions ---
@@ -116,26 +157,29 @@ func local_map_to_world(cell: Vector2i) -> Vector2:
 
 
 func _draw() -> void:
+	if infinite:
+		return
+
 	if not debug_enabled:
 		return
 
-	for cell in grid.keys():
+	for cell in _grid.keys():
 		var cell_in_world = local_map_to_world(cell)
 		var rect: Rect2 = Rect2(cell_in_world, cell_size)
 
-		draw_rect(rect, debug_grid_color, false)
+		draw_rect(rect, debug__grid_color, false)
 
 		if debug_show_labels:
-			var string: String = "%s\n%s" % [cell, grid[cell]]
+			var string: String = "%s\n%s" % [cell, _grid[cell]]
 			var label_font_size := min(cell_size.x * 0.5, 12)
 			var y_offset: float = cell_size.y * 0.5
 			var string_pos: Vector2 = cell_in_world + Vector2(0, y_offset)
-			var value_string_pos: Vector2 = string_pos + Vector2(0, label_font_size)
+			var value_string_pos: Vector2 = string_pos + Vector2(0, debug_font_size)
 
 			# Position string and value string below it.
-			draw_string(_debug_font, string_pos,
+			draw_string(debug_font, string_pos,
 						str(cell), HORIZONTAL_ALIGNMENT_CENTER,
-						cell_size.x, label_font_size, debug_grid_color)
-			draw_string(_debug_font, value_string_pos,
-						str(grid[cell]), HORIZONTAL_ALIGNMENT_CENTER,
-						cell_size.x, label_font_size, debug_grid_color)
+						cell_size.x, debug_font_size, debug__grid_color)
+			draw_string(debug_font, value_string_pos,
+						str(_grid[cell]), HORIZONTAL_ALIGNMENT_CENTER,
+						cell_size.x, debug_font_size, debug__grid_color)
--- addons/criss-cross/nodes/grid3d.gd
+++ addons/criss-cross/nodes/grid3d.gd
@@ -2,27 +2,35 @@
 extends Node3D
 
 
-@export var size: Vector2i = Vector2i(16, 16) : set = set_size
+@export var infinite: bool = false :
+	set(value):
+		infinite = value
+		reset()
+@export var regions: Array[Rect2i] = [Rect2i(0, 0, 16, 16)] :
+	set(value):
+		regions = value
+		reset()
 @export var cell_size: Vector2i = Vector2i(16, 16)
 
-var grid: Dictionary
+var _grid: Dictionary
 
 
 func _ready() -> void:
 	reset()
 
 
-func set_size(value: Vector2i) -> void:
-	size = value
-	reset()
-
-
 func reset() -> void:
-	grid.clear()
+	_grid.clear()
 
-	for x in size.x:
-		for z in size.y:
-			grid[Vector2i(x, z)] = null
+	if infinite:
+		return
+
+	for region in regions:
+		var x_range = range(region.position.x, region.position.x + region.size.x)
+		var z_range = range(region.position.y, region.position.y + region.size.y)
+		for x in x_range:
+			for z in z_range:
+				_grid[Vector2i(x, z)] = null
 
 
 # --- Values ---
@@ -30,11 +38,11 @@ func reset() -> void:
 
 ## Sets [param cell] to [param value]
 func set_value(cell: Vector2i, value) -> void:
-	if not has_cell(cell):
+	if not has_cell(cell) and not infinite:
 		push_warning("%s is out of %s's bounds" % [cell, name])
 		return
 
-	grid[cell] = value
+	_grid[cell] = value
 
 
 func set_valuexz(x: int, z: int, value) -> void:
@@ -49,10 +57,11 @@ func set_values(cells: Array[Vector2i], value) -> void:
 
 func get_value(cell: Vector2i):
 	if not has_cell(cell):
-		push_warning("%s is out of %s's bounds" % [cell, name])
+		if not infinite:
+			push_warning("%s is out of %s's bounds" % [cell, name])
 		return null
 
-	return grid[cell]
+	return _grid[cell]
 
 
 func get_valuexz(x: int, z: int):
@@ -62,11 +71,27 @@ func get_valuexz(x: int, z: int):
 # --- Checks ---
 
 func has_cell(cell: Vector2i) -> bool:
-	return grid.keys.has(cell)
+	return _grid.keys.has(cell)
 
 
 func has_cellxz(x: int, z: int) -> bool:
 	return has_cell(Vector2i(x, z))
+
+
+func is_value_null(cell: Vector2i) -> bool:
+	return get_value(cell) == null
+
+
+func is_value_nullxz(x: int, z: int) -> bool:
+	return is_value_null(Vector2i(x, z))
+
+
+func are_values_null(cells: Array[Vector2i]) -> bool:
+	for cell in cells:
+		if not is_value_null(cell):
+			return false
+
+	return true
 
 
 # --- Positions ---
@@ -74,7 +99,6 @@ func has_cellxz(x: int, z: int) -> bool:
 
 func world_to_map(pos: Vector3) -> Vector2i:
 	var pos_2d: Vector2 = Vector2(pos.x - position.x, pos.z - position.z)
-
 	return Vector2i(pos_2d / Vector2(cell_size))
 
 
@@ -83,7 +107,7 @@ func map_to_world_centered(cell: Vector2i) -> Vector3:
 
 
 func map_to_world(cell: Vector2i) -> Vector3:
-	return Vector3(cell.x * cell_size.x, position.y, cell.y * cell_size.y) + position
+	return Vector3(cell.x * cell_size.x, 0, cell.y * cell_size.y) + position
 
 
 func local_map_to_world(cell: Vector2i) -> Vector3:
